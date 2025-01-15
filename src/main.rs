33a4a9d02350e10927de98697275c6eecb2f62c5
Miguel <gomezmig03@gmail.com>
2025-01-15 23:13:42 +0100

    Add ball physics

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,24 @@ struct Player {
     speed: f32,
 }
 
+struct Ball {
+    position: Vector2,
+    speed: Vector2,
+    radius: f32,
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32)
         .title("Pong")
         .vsync()
         .build();
+    
+    let mut ball = Ball {
+        position: Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
+        speed: Vector2::new(2.5, 2.5),
+        radius: 20.0,
+    };
 
     let mut player1 = Player {
         position: Vector2::new(32.0, 32.0),
@@ -31,13 +43,42 @@ fn main() {
         speed: SCREEN_HEIGHT / 150.0,
     };
 
+    let mut points1: u32 = 0;
+    let mut points2: u32 = 0;
+
     while !rl.window_should_close() {
         // UPDATE
         if (rl.is_key_down(KEY_UP) || rl.is_key_down(KEY_W)) && player1.position.y > UP_LIMIT { player1.position.y -= player1.speed; }
         if (rl.is_key_down(KEY_DOWN) || rl.is_key_down(KEY_S)) && player1.position.y < DOWN_LIMIT { player1.position.y += player1.speed; }  
 
         if rl.is_key_down(KEY_O) && player2.position.y > UP_LIMIT { player2.position.y -= player2.speed; }
-        if rl.is_key_down(KEY_L) && player2.position.y < DOWN_LIMIT { player2.position.y += player2.speed; }  
+        if rl.is_key_down(KEY_L) && player2.position.y < DOWN_LIMIT { player2.position.y += player2.speed; } 
+
+        ball.position += ball.speed;
+
+        if ball.position.x <= ball.radius {
+            ball.speed.x *= -1.0;
+            points1 += 1; // puntos jugador 1
+        }
+
+        if ball.position.x >= SCREEN_WIDTH - ball.radius {
+            ball.speed.x *= -1.0;
+            points2 += 1; // puntos jugador 2
+        }
+
+        if ball.position.x <= player1.position.x + (player1.size.x + ball.radius) {
+            ball.speed.x *= -1.0;
+        }
+
+        if ball.position.x >= player2.position.x - ball.radius {
+            ball.speed.x *= -1.0;
+        }
+
+
+        if ball.position.y >= SCREEN_HEIGHT - ball.radius
+        || ball.position.y <= ball.radius {
+            ball.speed.y *= -1.0;
+        }
 
         //println!("Player position Y: {}", player1.position.y);
 
@@ -45,7 +86,9 @@ fn main() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::BLACK);
+
         d.draw_rectangle_v(player1.position, player1.size, Color::WHITE);
         d.draw_rectangle_v(player2.position, player2.size, Color::WHITE);
+        d.draw_circle_v(ball.position, ball.radius, Color::WHITE);
     }
 }
