b91da1feef03784a8b80b06e3b73a28d034cd7c0
Miguel <gomezmig03@gmail.com>
2025-01-16 00:27:00 +0100

    Add points and resets

--- src/main.rs
+++ src/main.rs
@@ -27,21 +27,32 @@ fn main() {
     
     let mut ball = Ball {
         position: Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
-        speed: Vector2::new(2.5, 2.5),
+        speed: Vector2::new(4.5, 2.5),
         radius: 20.0,
     };
 
     let mut player1 = Player {
         position: Vector2::new(32.0, 32.0),
         size: Vector2::new(SCREEN_WIDTH / 64.0, SCREEN_HEIGHT / 6.0),
-        speed: SCREEN_HEIGHT / 150.0,
+        speed: SCREEN_HEIGHT / 120.0,
     };
 
     let mut player2 = Player {
         position: Vector2::new(SCREEN_WIDTH - (SCREEN_WIDTH / 64.0) - 32.0, SCREEN_HEIGHT - (SCREEN_HEIGHT / 6.0) - 32.0),
         size: Vector2::new(SCREEN_WIDTH / 64.0, SCREEN_HEIGHT / 6.0),
-        speed: SCREEN_HEIGHT / 150.0,
+        speed: SCREEN_HEIGHT / 120.0,
     };
+
+
+    fn resetBall(ball: &mut Ball, currentSpeed: &mut f32, rl: &mut RaylibHandle) {
+        let mut yspeed = rl.get_random_value::<i32>(-4..4) as f32;
+        if yspeed == 0.0 { yspeed = 1.0 }
+
+        ball.position = Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0);
+        ball.speed = Vector2::new((rl.get_random_value::<i32>(3..5) as f32) * *currentSpeed, yspeed);
+    }
+
+    let mut currentSpeed: f32 = 1.0;
 
     let mut points1: u32 = 0;
     let mut points2: u32 = 0;
@@ -57,21 +68,29 @@ fn main() {
         ball.position += ball.speed;
 
         if ball.position.x <= ball.radius {
-            ball.speed.x *= -1.0;
-            points1 += 1; // puntos jugador 1
-        }
-
-        if ball.position.x >= SCREEN_WIDTH - ball.radius {
-            ball.speed.x *= -1.0;
+            currentSpeed = 1.0;
+            resetBall(&mut ball, &mut currentSpeed, &mut rl);
             points2 += 1; // puntos jugador 2
         }
 
-        if ball.position.x <= player1.position.x + (player1.size.x + ball.radius) {
-            ball.speed.x *= -1.0;
+        if ball.position.x >= SCREEN_WIDTH - ball.radius {
+            currentSpeed = -1.0;
+            resetBall(&mut ball, &mut currentSpeed, &mut rl);
+            points1 += 1; // puntos jugador 1
         }
 
-        if ball.position.x >= player2.position.x - ball.radius {
-            ball.speed.x *= -1.0;
+        if ball.position.x <= player1.position.x + (player1.size.x + ball.radius)
+        && (ball.position.y <= player1.position.y + player1.size.y + 4.0 
+        && ball.position.y >= player1.position.y)  { 
+            ball.speed.x *= -1.05;
+            ball.speed.y *= 1.05;
+        }
+
+        if ball.position.x >= player2.position.x - ball.radius
+        && (ball.position.y <= player2.position.y + player2.size.y + 4.0 
+        && ball.position.y >= player2.position.y){
+            ball.speed.x *= -1.05;
+            ball.speed.y *= 1.05;
         }
 
 
@@ -86,6 +105,9 @@ fn main() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::BLACK);
+
+        d.draw_text(&format!("Player 1: {points1}"), SCREEN_WIDTH as i32 / 16, 12, 24, Color::WHITE);
+        d.draw_text(&format!("Player 2: {points2}"), SCREEN_WIDTH as i32 - 256, 12, 24, Color::WHITE);
 
         d.draw_rectangle_v(player1.position, player1.size, Color::WHITE);
         d.draw_rectangle_v(player2.position, player2.size, Color::WHITE);
