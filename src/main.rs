be490a25d3eb8771868d6124f900efa6201d6501
Miguel <gomezmig03@gmail.com>
2025-01-15 22:36:21 +0100

    Add second player

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,19 @@ fn main() {
         speed: SCREEN_HEIGHT / 150.0,
     };
 
+    let mut player2 = Player {
+        position: Vector2::new(SCREEN_WIDTH - (SCREEN_WIDTH / 64.0) - 32.0, SCREEN_HEIGHT - (SCREEN_HEIGHT / 6.0) - 32.0),
+        size: Vector2::new(SCREEN_WIDTH / 64.0, SCREEN_HEIGHT / 6.0),
+        speed: SCREEN_HEIGHT / 150.0,
+    };
+
     while !rl.window_should_close() {
         // UPDATE
         if (rl.is_key_down(KEY_UP) || rl.is_key_down(KEY_W)) && player1.position.y > UP_LIMIT { player1.position.y -= player1.speed; }
         if (rl.is_key_down(KEY_DOWN) || rl.is_key_down(KEY_S)) && player1.position.y < DOWN_LIMIT { player1.position.y += player1.speed; }  
+
+        if rl.is_key_down(KEY_O) && player2.position.y > UP_LIMIT { player2.position.y -= player2.speed; }
+        if rl.is_key_down(KEY_L) && player2.position.y < DOWN_LIMIT { player2.position.y += player2.speed; }  
 
         //println!("Player position Y: {}", player1.position.y);
 
@@ -37,5 +46,6 @@ fn main() {
 
         d.clear_background(Color::BLACK);
         d.draw_rectangle_v(player1.position, player1.size, Color::WHITE);
+        d.draw_rectangle_v(player2.position, player2.size, Color::WHITE);
     }
 }
