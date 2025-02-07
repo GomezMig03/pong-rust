1cf16aab1f05a3ade9887003477288fd503b83a7
Miguel <gomezmig03@gmail.com>
2025-02-07 18:10:42 +0100

    fix functions names to snake case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use raylib::prelude::*;
 use raylib::consts::KeyboardKey::*;
+use raylib::prelude::*;
 
 const SCREEN_WIDTH: f32 = 1280.0;
 const SCREEN_HEIGHT: f32 = 720.0;
@@ -24,7 +24,7 @@ fn main() {
         .title("Pong")
         .vsync()
         .build();
-    
+
     let mut ball = Ball {
         position: Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
         speed: Vector2::new(4.5, 2.5),
@@ -38,21 +38,28 @@ fn main() {
     };
 
     let mut player2 = Player {
-        position: Vector2::new(SCREEN_WIDTH - (SCREEN_WIDTH / 64.0) - 32.0, SCREEN_HEIGHT - (SCREEN_HEIGHT / 6.0) - 32.0),
+        position: Vector2::new(
+            SCREEN_WIDTH - (SCREEN_WIDTH / 64.0) - 32.0,
+            SCREEN_HEIGHT - (SCREEN_HEIGHT / 6.0) - 32.0,
+        ),
         size: Vector2::new(SCREEN_WIDTH / 64.0, SCREEN_HEIGHT / 6.0),
         speed: SCREEN_HEIGHT / 120.0,
     };
 
-
-    fn resetBall(ball: &mut Ball, currentSpeed: &mut f32, rl: &mut RaylibHandle) {
+    fn reset_ball(ball: &mut Ball, currentSpeed: &mut f32, rl: &mut RaylibHandle) {
         let mut yspeed = rl.get_random_value::<i32>(-4..4) as f32;
-        if yspeed == 0.0 { yspeed = 1.0 }
+        if yspeed == 0.0 {
+            yspeed = 1.0
+        }
 
         ball.position = Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0);
-        ball.speed = Vector2::new((rl.get_random_value::<i32>(3..5) as f32) * *currentSpeed, yspeed);
+        ball.speed = Vector2::new(
+            (rl.get_random_value::<i32>(3..5) as f32) * *currentSpeed,
+            yspeed,
+        );
     }
 
-    let mut currentSpeed: f32 = 1.0;
+    let mut current_speed: f32 = 1.0;
 
     let mut points1: u32 = 0;
     let mut points2: u32 = 0;
@@ -61,29 +68,38 @@ fn main() {
 
     while !rl.window_should_close() {
         // UPDATE
-        if (rl.is_key_down(KEY_UP) || rl.is_key_down(KEY_W)) && player1.position.y > UP_LIMIT { player1.position.y -= player1.speed; }
-        if (rl.is_key_down(KEY_DOWN) || rl.is_key_down(KEY_S)) && player1.position.y < DOWN_LIMIT { player1.position.y += player1.speed; }  
+        if (rl.is_key_down(KEY_UP) || rl.is_key_down(KEY_W)) && player1.position.y > UP_LIMIT {
+            player1.position.y -= player1.speed;
+        }
+        if (rl.is_key_down(KEY_DOWN) || rl.is_key_down(KEY_S)) && player1.position.y < DOWN_LIMIT {
+            player1.position.y += player1.speed;
+        }
 
-        if rl.is_key_down(KEY_O) && player2.position.y > UP_LIMIT { player2.position.y -= player2.speed; }
-        if rl.is_key_down(KEY_L) && player2.position.y < DOWN_LIMIT { player2.position.y += player2.speed; } 
+        if rl.is_key_down(KEY_O) && player2.position.y > UP_LIMIT {
+            player2.position.y -= player2.speed;
+        }
+        if rl.is_key_down(KEY_L) && player2.position.y < DOWN_LIMIT {
+            player2.position.y += player2.speed;
+        }
 
         ball.position += ball.speed;
 
         if ball.position.x <= ball.radius {
-            currentSpeed = 1.0;
-            resetBall(&mut ball, &mut currentSpeed, &mut rl);
+            current_speed = 1.0;
+            reset_ball(&mut ball, &mut current_speed, &mut rl);
             points2 += 1; // puntos jugador 2
         }
 
         if ball.position.x >= SCREEN_WIDTH - ball.radius {
-            currentSpeed = -1.0;
-            resetBall(&mut ball, &mut currentSpeed, &mut rl);
+            current_speed = -1.0;
+            reset_ball(&mut ball, &mut current_speed, &mut rl);
             points1 += 1; // puntos jugador 1
         }
 
         if ball.position.x <= player1.position.x + (player1.size.x + ball.radius)
-        && (ball.position.y <= player1.position.y + player1.size.y + 4.0 
-        && ball.position.y >= player1.position.y)  { 
+            && (ball.position.y <= player1.position.y + player1.size.y + 4.0
+                && ball.position.y >= player1.position.y)
+        {
             if hit == 0 {
                 ball.speed.x *= -1.05;
                 ball.speed.y *= 1.05;
@@ -92,23 +108,26 @@ fn main() {
         }
 
         if ball.position.x >= player2.position.x - ball.radius
-        && (ball.position.y <= player2.position.y + player2.size.y + 4.0 
-        && ball.position.y >= player2.position.y){
+            && (ball.position.y <= player2.position.y + player2.size.y + 4.0
+                && ball.position.y >= player2.position.y)
+        {
             if hit == 0 {
                 ball.speed.x *= -1.05;
                 ball.speed.y *= 1.05;
             }
-            hit +=1;
+            hit += 1;
         }
 
-
-        if ball.position.y >= SCREEN_HEIGHT - ball.radius
-        || ball.position.y <= ball.radius {
+        if ball.position.y >= SCREEN_HEIGHT - ball.radius || ball.position.y <= ball.radius {
             ball.speed.y *= -1.0;
         }
 
-        if hit != 0 {hit+=1;}
-        if hit >= 15 {hit=0;}
+        if hit != 0 {
+            hit += 1;
+        }
+        if hit >= 15 {
+            hit = 0;
+        }
 
         //println!("Player position Y: {}", player1.position.y);
 
@@ -117,8 +136,20 @@ fn main() {
 
         d.clear_background(Color::BLACK);
 
-        d.draw_text(&format!("Player 1: {points1}"), SCREEN_WIDTH as i32 / 16, 12, 24, Color::WHITE);
-        d.draw_text(&format!("Player 2: {points2}"), SCREEN_WIDTH as i32 - 256, 12, 24, Color::WHITE);
+        d.draw_text(
+            &format!("Player 1: {points1}"),
+            SCREEN_WIDTH as i32 / 16,
+            12,
+            24,
+            Color::WHITE,
+        );
+        d.draw_text(
+            &format!("Player 2: {points2}"),
+            SCREEN_WIDTH as i32 - 256,
+            12,
+            24,
+            Color::WHITE,
+        );
 
         d.draw_rectangle_v(player1.position, player1.size, Color::WHITE);
         d.draw_rectangle_v(player2.position, player2.size, Color::WHITE);
